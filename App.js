9ac0605a7254275519a700b765fbb5215c660a92
Vinicius Lima <vinicius@loldesign.com.br>
2020-01-16 19:42:31 -0300

    Load Routes on App.js

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Routes />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159C1',
     alignItems: 'center',
     justifyContent: 'center',
   },
